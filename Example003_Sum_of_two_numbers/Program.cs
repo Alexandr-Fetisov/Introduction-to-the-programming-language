752d0dc5fa7a01ebe00b614443053b7bb034967a
Alexandr Fetisov <fav13233@mail.ru>
2022-09-14 19:07:50 +0300

    Example 003 added new Random

--- Example003_Sum_of_two_numbers/Program.cs
+++ Example003_Sum_of_two_numbers/Program.cs
@@ -1,4 +1,7 @@
-﻿int numberA = 5;
-int numberB = 5;
+﻿//Добавили команду new Random().Next(min,max) для генерирования случайных чисел, в данном примере от 1 до 10
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA); //Добавили команду Console.WriteLine() для вывода сгенерированного числа в терминал
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
