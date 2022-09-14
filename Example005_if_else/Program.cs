e568f9a66c87cd223d16616dd066a6b77fe4b0b8
Alexandr Fetisov <fav13233@mail.ru>
2022-09-14 19:46:00 +0300

    Example 005 if and else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alexandr") // в концу после закрывающей скобки ) нет точки с запятой; добавили ToLower() - позволяет все символы строки перевести в нижний регистр
+{
+    Console.WriteLine("Ура, это же Alexandr!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
