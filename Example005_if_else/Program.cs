3c58203122624fca59e002e9972583adeda7bb78
Alexandr Fetisov <fav13233@mail.ru>
2022-09-17 15:35:56 +0300

    откорректированы Example

--- Example005_if_else/Program.cs
+++ Example005_if_else/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "alexandr") // в концу после закрывающей скобки ) нет точки с запятой; добавили ToLower() - позволяет все символы строки перевести в нижний регистр
+if(username.ToLower() == "alexandr") // в концe после закрывающей скобки ) нет точки с запятой; добавили ToLower() - позволяет все символы строки перевести в нижний регистр
 {
     Console.WriteLine("Ура, это же Alexandr!");
 }
